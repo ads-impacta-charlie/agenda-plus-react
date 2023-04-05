12cadf6c155ece8b2f01736d5489458468a6d382
Bruno Ferreira <bruno..rferreira@aluno.faculdadeimpacta.com.br>
2023-04-05 20:36:53 -0300

    Implementa remoção de um dado de contato

--- src/app/components/create-contact-form.tsx
+++ src/app/components/create-contact-form.tsx
@@ -2,11 +2,13 @@
 
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {ContactData} from "@/app/entity/contact-data";
 import CreateContactData from "@/app/components/create-contact-data";
-import {Button} from "@mui/material";
+import {Button, Grid} from "@mui/material";
 import {Contact} from "@/app/entity/contact";
+import CreateContactDataMenu from "@/app/components/create-contact-data-menu";
+import {v4} from 'uuid';
 
 interface CreateContactFormParams {
   onSave: (contact: Contact) => void
@@ -24,7 +26,7 @@ export default function CreateContactForm({onSave, contact}: CreateContactFormPa
 
   const handleAddDataClick = () => {
     setData(data => {
-      return [...data, {}]
+      return [...data, {uuid: v4()}]
     })
   }
 
@@ -34,8 +36,16 @@ export default function CreateContactForm({onSave, contact}: CreateContactFormPa
     setData(newData)
   }
 
+  const handleOnDeleteContactData = (deleted: ContactData, index: number) => {
+    const newData = data
+      .filter((d, i) => i != index)
+      .map(d => ({...d}))
+    setData(newData)
+  }
+
   const handleSaveClick = () => {
-    const contactToSave: Contact = { name, data, uuid: contact?.uuid }
+
+    const contactToSave: Contact = { name, data: data.map(d => ({...d, uuid: undefined})), uuid: contact?.uuid }
     const headers = new Headers()
     headers.set('accept', 'application/json')
     headers.set('content-type', 'application/json')
@@ -56,7 +66,14 @@ export default function CreateContactForm({onSave, contact}: CreateContactFormPa
   return (<Box component="form" autoComplete="off">
     <TextField required variant="outlined" label="Nome" value={name} onChange={e => setName(e.target.value)}></TextField>
     {data.map((d, i) => (
-      <CreateContactData key={d.uuid} data={d} dataIndex={i} onChange={handleChangeContactData}></CreateContactData>
+      <Grid key={d.uuid} container spacing={2}>
+        <Grid item xs={12} md={10}>
+          <CreateContactData data={d} dataIndex={i} onChange={handleChangeContactData}></CreateContactData>
+        </Grid>
+        <Grid item xs={12} md={2}>
+          <CreateContactDataMenu data={d} index={i} onDelete={handleOnDeleteContactData} />
+        </Grid>
+      </Grid>
     ))}
     <Button onClick={handleAddDataClick} variant="outlined">Adicionar Dados</Button>
     <Button onClick={handleSaveClick} variant="contained">Salvar</Button>
